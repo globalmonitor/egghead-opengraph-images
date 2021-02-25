5a1d832ea57d1f93ff9966da571015a3afb11e91
fashly <hello@sowmith.dev>
2021-02-25 07:32:52 -0800

    added cloudinary cache 2

--- functions/process-url/process-url.js
+++ functions/process-url/process-url.js
@@ -10,31 +10,21 @@ exports.handler = async function(event, ctx) {
   const { queryStringParameters } = event;
   console.log(queryStringParameters);
   try {
-    // https://res.cloudinary.com/sector/image/upload/v1583637123/og-images/img-1.png
-    const imageUrl = cloudinary.url(
-      `${process.env.REPORT_IMAGE_VERSION}/og-images/FF4D00-0.8_d2ut0o.png`,
-      {
-        // resouce_type: "raw"
-        sign_url: true,
-        // secure: true,
-        custom_pre_function: {
-          function_type: "remote",
-          source: `https://peaceful-shannon-670b41.netlify.app/.netlify/functions/gen-opengraph-image?${qs.stringify(
-            queryStringParameters
-          )}`
-        }
-      }
-    );
-    console.log(`https://peaceful-shannon-670b41.netlify.app/.netlify/functions/gen-opengraph-image?${qs.stringify(
-            queryStringParameters
-          )}`)
+
+    const imageUrl = `https://res.cloudinary.com/${
+      process.env.CLOUD_NAME
+    }/image/fetch/${encodeURIComponent(
+      `https://peaceful-shannon-670b41.netlify.app/.netlify/functions/gen-opengraph-image?${qs.stringify(
+        queryStringParameters,
+      )}`,
+    )}`
     return {
       statusCode: 302,
       headers: {
-        Location: imageUrl
+        Location: imageUrl,
       },
-      body: ""
-    };
+      body: '',
+    }
   } catch (e) {
     console.log(e);
   }
